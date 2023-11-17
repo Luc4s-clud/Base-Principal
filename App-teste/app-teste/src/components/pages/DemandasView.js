2afc805f11bdb3cefbc65d8f7d32acbd29a277d4
Lucas Rodrigues <lucasrodrigues3210@gmail.com>
2023-11-17 16:51:44 -0300

    Demandas

--- App-teste/app-teste/src/components/pages/DemandasView.js
+++ App-teste/app-teste/src/components/pages/DemandasView.js
@@ -1,25 +1,14 @@
 import React from 'react';
+import './DemandasView.css'; // Certifique-se de criar este arquivo CSS para estilizar seu componente
 
 function DemandasView({ demandas }) {
-  // Se demandas for null ou indefinido, renderize uma mensagem ou um loader
-  if (!demandas) {
-    return <div>Carregando demandas...</div>;
-  }
-
-  // Se não houver demandas, renderize uma mensagem apropriada
-  if (demandas.length === 0) {
-    return <div>Nenhuma demanda encontrada.</div>;
-  }
-
-  // Renderize a lista de demandas
   return (
     <div className="demandas-container">
-      <h1>Demandas</h1>
       {demandas.map((demanda) => (
-        <div key={demanda.cd_demanda} className="demanda">
-          <h2>{demanda.nm_demanda}</h2>
-          <p>{demanda.ds_descricao}</p>
-          {/* Inclua outros campos conforme necessário */}
+        <div key={demanda.cd_demanda} className="demanda-card">
+          <div className="demanda-titulo">{demanda.nm_demanda}</div>
+          <div className="demanda-descricao">{demanda.ds_descricao}</div>
+          <button className="demanda-inscrever">Inscrever-se</button>
         </div>
       ))}
     </div>
