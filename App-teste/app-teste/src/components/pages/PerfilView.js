a27eb348fd654ecabc3454f2ef3c4c84a25074dd
Lucas Rodrigues <110870612+Julio7k@users.noreply.github.com>
2023-11-18 10:52:07 -0300

    Update PerfilView.js

--- App-teste/app-teste/src/components/pages/PerfilView.js
+++ App-teste/app-teste/src/components/pages/PerfilView.js
@@ -5,7 +5,7 @@ function PerfilView({ user }) {
     <div className="perfil-container">
       <h1>Perfil</h1>
       <div className="perfil-card">
-        <p><strong>Nome</strong>{user.nm_usuario}</p>
+        <p><strong>Nome:</strong>{user.nm_usuario}</p>
         <p><strong>Email:</strong> {user.ds_email}</p>
         <p><strong>Telefone:</strong> {user.nr_telefone}</p>
         <p><strong>CPF:</strong> {user.nr_cpf}</p>
