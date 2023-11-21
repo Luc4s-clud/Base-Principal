e03b7141739b20dec645b0e0d679f7555b779a0d
Lucas Rodrigues <110870612+Julio7k@users.noreply.github.com>
2023-11-21 12:06:40 -0300

    Pagina certificados / barra de atividades
    Começo da construção da barra de certificados e barra da atividades para pagina home

--- App-teste/app-teste/src/components/pages/Certificados.js
+++ App-teste/app-teste/src/components/pages/Certificados.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Certificados.css';
 
 function Certificados() {
   const navigate = useNavigate();
@@ -12,9 +13,12 @@ function Certificados() {
   }, [navigate]);
 
   return (
-    <div>
+    <div className="certificados-container">
       <h1>Certificados</h1>
-      {/* Restante do conteúdo da página Certificados */}
+      <p className="certificados-texto">Clique no botão abaixo para baixar seu certificado</p>
+      <a href="../images/certificado.png" download="certificado.png">
+        <button className="botao-certificado">Baixar Certificado</button>
+      </a>
     </div>
   );
 }
