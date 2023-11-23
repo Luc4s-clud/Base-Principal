15ee1bacba92b0bf9767fce02ee702fb20b783ea
Lucas Rodrigues <84715912+Luc4s-clud@users.noreply.github.com>
2023-11-23 19:54:51 -0300

    Teste

--- App-teste/routes/routes.js
+++ App-teste/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../Controllers/UserController');
 const DemandaController = require('../Controllers/DemandaController');
-
+const LocalizacaoController = require('../Controllers/LocalizacaoController');
 
 
 // Exemplo de rota
@@ -14,6 +14,7 @@ router.post('/register', UserController.createUser);
 router.get('/user/:id', UserController.getUser);
 router.get('/demandas', DemandaController.getAllDemandas);
 router.post('/demandas', DemandaController.createDemanda);
+router.get('/localizacao', LocalizacaoController.listAll);
 
 
 // Aqui, você adicionará mais rotas conforme necessário
