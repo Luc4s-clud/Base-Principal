c428e6923962cb5dfffc501cc6979af2218d97df
Lucas Rodrigues <lucasrodrigues3210@gmail.com>
2023-11-24 07:07:32 -0300

    Alterações que eu fiz certas

--- App-teste/models/localizacao.js
+++ App-teste/models/localizacao.js
@@ -1,12 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
-    const Localizacao = sequelize.define('Localizacao', {
-      cd_localizacao: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-      ds_localizacao: DataTypes.STRING,
-    }, {
-      tableName: 'localizacao',
-      timestamps: false // Desativar timestamps se não estiver usando campos createdAt e updatedAt
-    });
-  
-    return Localizacao;
-  };
-  
+  const Localizacao = sequelize.define('Localizacao', {
+    cd_localizacao: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    ds_localizacao: DataTypes.STRING,
+  }, {
+    tableName: 'localizacao',
+    timestamps: false
+  });
+
+  return Localizacao;
+};
