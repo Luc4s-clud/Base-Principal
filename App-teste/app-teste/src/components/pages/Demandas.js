1b7233e812073bafaea1dc9e8b65c60a510f7a23
Lucas Rodrigues <110870612+Julio7k@users.noreply.github.com>
2023-11-28 09:59:52 -0300

    Update Demandas.js

--- App-teste/app-teste/src/components/pages/Demandas.js
+++ App-teste/app-teste/src/components/pages/Demandas.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import DemandasView from './DemandasView';
 import { useNavigate } from 'react-router-dom';
+import './Demandas.css';
 
 function Demandas() {
   const [demandas, setDemandas] = useState([]);
