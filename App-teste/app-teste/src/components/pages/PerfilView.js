63ecc9b780eeca40195d8ffe2d5e3c8ede6302df
Lucas Rodrigues <110870612+Julio7k@users.noreply.github.com>
2023-12-07 17:21:01 -0300

    CSS para perfil

--- App-teste/app-teste/src/components/pages/PerfilView.js
+++ App-teste/app-teste/src/components/pages/PerfilView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Perfil.css';
 
 function PerfilView({ user }) {
   const navigate = useNavigate();
@@ -11,10 +12,10 @@ function PerfilView({ user }) {
     <div className="perfil-container">
       <h1>Perfil</h1>
       <div className="perfil-card">
-        <p><strong>Nome:</strong>{user.nm_usuario}</p>
-        <p><strong>Email:</strong> {user.ds_email}</p>
-        <p><strong>Telefone:</strong> {user.nr_telefone}</p>
-        <p><strong>CPF:</strong> {user.nr_cpf}</p>
+        <p><strong>Nome: </strong>{user.nm_usuario}</p>
+        <p><strong>Email: </strong> {user.ds_email}</p>
+        <p><strong>Telefone: </strong> {user.nr_telefone}</p>
+        <p><strong>CPF: </strong> {user.nr_cpf}</p>
         <button onClick={handleLogout} className="logout-button" style={{ width: '100px', margin: '10px' }}>Sair</button>
       </div>
     </div>
